1a71baf38b1dd86f61f64ee6f235d50e7d6a6987
C-oss <52643215+C-oss@users.noreply.github.com>
2021-05-29 19:17:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,7 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+
+git clone https://github.com/i028/luci-theme-netgear
+rm -rf ../lean/luci-theme-netgear
